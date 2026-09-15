6ae14ce2abfc9c0a2b7ae256d253696d7510696c
dssfgs <48532311+dssfgs@users.noreply.github.com>
2026-09-15 20:32:26 +0800

    Cache app3.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "banwu-v2";
-const ASSETS = ["./", "./index.html", "./app1.js", "./app2.js", "./manifest.json", "./icon.svg"];
+const CACHE = "banwu-v3";
+const ASSETS = ["./", "./index.html", "./app1.js", "./app2.js", "./app3.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
